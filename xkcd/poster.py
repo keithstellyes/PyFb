50a6ff8babf08c2678d706e18771f6b677d02cea
Keith Stellyes <keithastellyes@gmail.com>
2020-05-19 08:47:37 -0700

    XKCD, post url

--- xkcd/poster.py
+++ xkcd/poster.py
@@ -9,7 +9,12 @@ def get_post_content():
 	chosen_num = random.randint(1, latest_comic_num)
 	print('Picking comic, 1 <= {} <= {}'.format(chosen_num, latest_comic_num))
 	comic = xkcd.get_comic(chosen_num, CACHE)
-	content = '#{} Title: {}\nAlt:{}'.format(comic['num'], comic['safe_title'], comic['alt'])
+	content = 'https://xkcd.com/{}/\n#{} Title: {}\nAlt:{}'.format(comic['num'], comic['num'], comic['safe_title'], comic['alt'])
 	if 'transcript' in comic.keys() and len(comic['transcript']) > 0:
 		content += '\nTranscript:{}'.format(comic['transcript'])
-	return content
+	return content, xkcd.ensure_comic_image_path(comic)
+
+if __name__ == '__main__':
+	fb_client = PyFb(json.load(open('../tokens.json', 'r'))['xkcd'])
+	text, image_path = get_post_content()
+	fb_client.create_post(message=text, image_path=image_path)
