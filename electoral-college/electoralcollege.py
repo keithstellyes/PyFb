3c25f2c0fddbfc9c5d8447147259076816a47726
Keith Stellyes <keithastellyes@gmail.com>
2020-05-22 09:29:56 -0700

    Electoral college bot cleanup

--- electoral-college/electoralcollege.py
+++ electoral-college/electoralcollege.py
@@ -1,4 +1,4 @@
-# via https://github.com/matplotlib/basemap/tree/master/examples
+# heavily modified from via https://github.com/matplotlib/basemap/tree/master/examples
 from __future__ import (absolute_import, division, print_function)
 
 import numpy as np
