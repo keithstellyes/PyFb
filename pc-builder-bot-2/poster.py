61d3745b9f782377f581e7cd26fb5aa4a60ab7ad
EC2 Default User <ec2-user@ip-172-31-26-48.us-west-2.compute.internal>
2020-08-15 18:49:41 +0000

    model typo fix

--- pc-builder-bot-2/poster.py
+++ pc-builder-bot-2/poster.py
@@ -6,4 +6,4 @@ from angusphotos.ap import random_angus_photo_file_path
 
 fb_client = PyFb(json.load(open('../tokens.json', 'r'))['pcbuilder'])
 
-fb_client.create_post(message=model.pstr_pc(mode.random_pc()), image_path=random_angus_photo_file_path())
+fb_client.create_post(message=model.pstr_pc(model.random_pc()), image_path=random_angus_photo_file_path())
